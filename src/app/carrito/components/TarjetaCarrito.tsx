2df3865338c80120827dc3f11b8f05f46eb45049
lk3v1nn <kevinlmorataya@gmail.com>
2023-11-02 21:36:11 -0600

    das

--- src/app/carrito/components/TarjetaCarrito.tsx
+++ src/app/carrito/components/TarjetaCarrito.tsx
@@ -47,6 +47,7 @@ export default function TarjetaCarrito(prop: {
                     className="relative  px-4 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-blue-300 bg-opacity-50"
                     onClick={() => {
                         setIdProducto(prop.id);
+                        console.log(prop.id)
                         eliminarProducto();
                     }}
                 >
